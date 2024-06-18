8bc3520a6aa81879519311e7313b80066b547fe4
EstafaniaDiaz14 <160290649+EstafaniaDiaz14@users.noreply.github.com>
2024-06-17 21:06:37 -0600

    version 6

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include "C:/Users/Usuario/Documents/GitHub/tetrisproyecto/include/Board.hpp"
-#include "C:/Users/Usuario/Documents/GitHub/tetrisproyecto/include/UI.hpp"
-#include "C:/Users/Usuario/Documents/GitHub/tetrisproyecto/include/Sounds.hpp"
-//#include "C:/Users/diequ/OneDrive/Documentos/GitHub/tetrisproyecto/include/Board.hpp"
-//#include "C:/Users/diequ/OneDrive/Documentos/GitHub/tetrisproyecto/include/UI.hpp"
-//#include "C:/Users/diequ/OneDrive/Documentos/GitHub/tetrisproyecto/include/Sounds.hpp"
+#include <Board.hpp>
+#include <UI.hpp>
+#include <Sounds.hpp>
 #include <fstream>
 
 using namespace std;
