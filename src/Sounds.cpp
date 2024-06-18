321c9f3fc9fe80b0b55f444dcbc0b7885fe56531
EstafaniaDiaz14 <160290649+EstafaniaDiaz14@users.noreply.github.com>
2024-06-17 22:47:18 -0600

    version 7

--- src/Sounds.cpp
+++ src/Sounds.cpp
@@ -1,19 +1,19 @@
 #include <Sounds.hpp>
 
 Sounds::Sounds(){
-	music.openFromFile(/*"C:/Users/diequ/OneDrive/Documentos/GitHub/tetrisproyecto/assets/music/music.ogg"*/"C:/Users/Usuario/Documents/GitHub/tetrisproyecto/assets/music/music.ogg");
+	music.openFromFile("./assets/music/music.ogg");
 	music.setLoop(true);
 	music.setVolume(20);
 	
-	if(!lineSoundBuffer.loadFromFile(/*"C:/Users/diequ/OneDrive/Documentos/GitHub/tetrisproyecto/assets/music/line.ogg"*/"C:/Users/Usuario/Documents/GitHub/tetrisproyecto/assets/music/line.ogg")){
+	if(!lineSoundBuffer.loadFromFile("./assets/music/line.ogg")){
 		cout<<"Error al cargar sonido"<<endl;
 	}
 	
-	if(!newScoreSoundBuffer.loadFromFile(/*"C:/Users/diequ/OneDrive/Documentos/GitHub/tetrisproyecto/assets/music/newScore.ogg"*/"C:/Users/Usuario/Documents/GitHub/tetrisproyecto/assets/music/newScore.ogg")){
+	if(!newScoreSoundBuffer.loadFromFile("./assets/music/newScore.ogg")){
 		cout<<"Error al cargar sonido"<<endl;
 	}
 	
-	if(!gameOverSoundBuffer.loadFromFile(/*"C:/Users/diequ/OneDrive/Documentos/GitHub/tetrisproyecto/assets/music/gameOver.ogg"*/"C:/Users/Usuario/Documents/GitHub/tetrisproyecto/assets/music/gameOver.ogg")){
+	if(!gameOverSoundBuffer.loadFromFile("./assets/music/gameOver.ogg")){
 		cout<<"Error al cargar sonido"<<endl;
 	}
 	lineSound.setBuffer(lineSoundBuffer);
